8435a9000f8f8ff7880dc369e4fea9c01a754d54
pananon <pandeyjss01@gmail.com>
2021-12-06 12:52:34 +0530

    Navbar Component created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import {Link} from 'react-router-dom'
-import { HomeOutlined, MoneyCollectOutLined, BulbOutLined , FundOutlined, MenuOutlined
-} from '@ant-design/icons/lib/icons'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined , FundOutlined} from '@ant-design/icons/lib/icons'
 
+import icon from '../images/btc.png'
 const Navbar = () => {
     return (
         <div className='nav-container'>
+            
             <div className='logo-container'>
-            <Avatar />
+            <Avatar src={icon} size="small" />
             <Typography.Title level={2} className= "logo">
                 <Link to="/">Crypto World</Link>
->            </Typography.Title>
-            <Button className='="Minu'
+            </Typography.Title>
             </div>
+
+            <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+            </Menu.Item>
+            </Menu>
+            
         </div>
-    )
+    );
 }
 
 export default Navbar
